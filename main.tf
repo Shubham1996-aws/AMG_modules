d5730b387974f0cec0b7ce5da739dced8b7aa6d8
DeepakshiPurushotham <107757971+DeepakshiPurushotham@users.noreply.github.com>
2022-10-20 14:27:20 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
 module "Grafana" {
     source = "./AMG"
     name = "Demo"
-    account_id = [ "282715913373","386110844697" ]
+    account_id = [ "" ]
     account_access_type = "CURRENT_ACCOUNT"
     authentication_providers = ["AWS_SSO"]
     permission_type = "CUSTOMER_MANAGED"
